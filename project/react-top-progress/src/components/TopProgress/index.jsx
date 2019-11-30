a832d812d1b50a150d477a2c4abe889e607a6aa5
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-11-30 05:19:23 -0300

    TopProgress initial styles

---
+++ project/react-top-progress/src/components/TopProgress/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import React from 'react';
+
+const TopProgress = () => {
+  return (
+    <div className="progress-container">
+      <div className="progress-level" />
+    </div>
+  );
+};
+
+export default TopProgress;
